934750eb50e616500469e04593bf0290e64abc88
Eluisky <luisabenojarandreu@gmail.com>
2023-11-20 13:29:42 +0100

    inicio mp3

---
+++ src/main/java/com/example/proyectomp3/Controlador.java
@@ -0,0 +1,14 @@
+package com.example.proyectomp3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controlador {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
